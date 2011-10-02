1baf4897663ccc8614ba8150816a345aa9f997c6
Gabriel Fortuna <gee.forr@gmail.com>
2011-10-02 16:08:22 +0200

    started working on fill function

--- exercise03_image_editor/lib/image_editor.rb
+++ exercise03_image_editor/lib/image_editor.rb
@@ -24,4 +24,65 @@ class ImageEditor
     def colour_pixel(row, col, colour)
         @grid[row - 1][col - 1] = colour
     end
+
+    def horizontal_segment(row, start, stop, colour)
+        adj_start = start - 1
+        adj_stop  = stop  - 1
+        @grid[row-1].fill(colour, adj_start..adj_stop)
+    end
+
+    def vertical_segment(col, start, stop, colour)
+        (start..stop).each do |row|
+            colour_pixel(row, col, colour)
+        end
+    end
+
+    def fill(row, col, colour)
+
+    end
+
+    private
+
+    def find_neighbours(row, col)
+        x          = row - 1
+        y          = col - 1
+        rows       = @grid.size
+        cols       = @grid[0].size
+        colour     = pixel(x, y)
+        neighbours = []
+        
+        # top neighbour
+        unless row == 1 # If we're not already on the top row
+            neighbours << [x - 1, y] if pixel(x - 1, y) == colour
+        end
+
+        # bottom neighbour
+        unless row == rows # If we're not already at the bottom row
+            neighbours << [x + 1, y] if 
+        end
+
+        # left neighbour
+        unless col == 1 # If we're not already on the left-most column
+            neighbours << [x, y - 1]
+        end
+
+        # right neighbour
+        unless col == cols # If we're not already on the right most column
+            neighbours << [x, y + 1]
+        end
+    end
+
+    def get_pixel_colour(x, y) # Note, 0-indexed
+        @grid[x][y]
+    end
 end
+
+
+
+
+
+
+
+
+
+
--- exercise03_image_editor/spec/test_image_editor_spec.rb
+++ exercise03_image_editor/spec/test_image_editor_spec.rb
@@ -7,34 +7,44 @@ require './lib/image_editor'
 
 describe ImageEditor do
   before(:each) do
-    @grid       = ImageEditor.new(5, 4)
+    @image       = ImageEditor.new(5, 4)
     @clear_grid = "OOOOO\nOOOOO\nOOOOO\nOOOOO\n"
   end
 
   it "should be an ImageEditor object" do
-      @grid.must_be_instance_of(ImageEditor)
+      @image.must_be_instance_of(ImageEditor)
   end
 
   it "should print a grid of O's" do
-      @grid.to_s.must_equal @clear_grid
+      @image.to_s.must_equal @clear_grid
   end
 
   it "should have a grid method that is an array of arrays" do
-      @grid.grid.must_equal [['O', 'O', 'O', 'O', 'O'],
-                             ['O', 'O', 'O', 'O', 'O'],
-                             ['O', 'O', 'O', 'O', 'O'],
-                             ['O', 'O', 'O', 'O', 'O']]
+      @image.grid.must_equal [['O', 'O', 'O', 'O', 'O'],
+                              ['O', 'O', 'O', 'O', 'O'],
+                              ['O', 'O', 'O', 'O', 'O'],
+                              ['O', 'O', 'O', 'O', 'O']]
   end
 
   describe "editing features" do
       it "should be able to fill a single pixel" do
-          @grid.colour_pixel(3, 2, "X")
-          @grid.to_s.must_equal "OOOOO\nOOOOO\nOXOOO\nOOOOO\n"
+          @image.colour_pixel(3, 2, "X")
+          @image.to_s.must_equal "OOOOO\nOOOOO\nOXOOO\nOOOOO\n"
       end
 
       it "should be able to clear the grid" do
-          @grid.clear
-          @grid.to_s.must_equal @clear_grid
+          @image.clear
+          @image.to_s.must_equal @clear_grid
+      end
+
+      it "should be able to fill a sub-row with a colour" do
+          @image.horizontal_segment(3, 2, 4, "H")
+          @image.to_s.must_equal "OOOOO\nOOOOO\nOHHHO\nOOOOO\n"
+      end
+
+      it "should be able to fill a sub-column with a colour" do
+          @image.vertical_segment(3, 2, 4, "V")
+          @image.to_s.must_equal "OOOOO\nOOVOO\nOOVOO\nOOVOO\n"
       end
   end
 end
