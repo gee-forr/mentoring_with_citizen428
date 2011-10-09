1c08fcec2ee25db0b12b47518cf850e66402da91
Gabriel Fortuna <gee.forr@gmail.com>
2011-10-09 13:35:43 +0200

    Lazy commit, fill is working
    Lots of little changes happened  here. I shoulda committed more often

--- exercise03_image_editor/lib/image_editor.rb
+++ exercise03_image_editor/lib/image_editor.rb
@@ -31,7 +31,7 @@ class ImageEditor
     def horizontal_segment(row, start, stop, colour)
         adj_start = start - 1
         adj_stop  = stop  - 1
-        @grid[row-1].fill(colour, adj_start..adj_stop)
+        @grid[row-1].fill(colour, (adj_start..adj_stop))
     end
 
     def vertical_segment(col, start, stop, colour)
@@ -41,86 +41,40 @@ class ImageEditor
     end
 
     def fill(row, col, colour)
-        @neighbours = find_neighbours(row, col)
+        x          = col - 1
+        y          = row - 1
+        old_colour = pixel(x, y)
 
-        walk_grid(row, col, colour)
-
-
+        flood_fill(x, y, old_colour, colour)
     end
 
-
-
+    # Private method pixels are referenced by 0-indexed coords
     private
-    
-    def walk_grid(row, col, colour)
-        begin
-            @neighbours.each do |x, y|
-                next if neighbours_found?(x, y)
-                n_row = x + 1
-                n_col = y + 1
-                binding.pry
 
-                @neighbours << find_neighbours(n_row, n_col)
-            end
-        end until found_all_neighbours?
-    end
+    def flood_fill(x, y, old_colour, new_colour)
+        return if pixel(x, y) != old_colour
 
-    def find_neighbours(row, col)
-        x          = row - 1
-        y          = col - 1
-        rows       = @grid.size
-        cols       = @grid[0].size
-        colour     = pixel(x, y)
-        neighbours = []
+        colour_pixel(y.next, x.next, new_colour) # colour pixel uses row, col, not x, y
+
+        row        = y.next
+        bottom     = @grid.size
+        col        = x.next
+        right_most = @grid[0].size
+
+        # Fill pixel above
+        flood_fill(x, y - 1, old_colour, new_colour) unless row == 1
         
-        # top neighbour
-        unless row == 1 # If we're not already on the top row
-            neighbours << [x - 1, y] if pixel(x - 1, y) == colour
-        end
+        # Fill pixel below
+        flood_fill(x, y + 1, old_colour, new_colour) unless row == bottom
+        
+        # Fill pixel to the left
+        flood_fill(x - 1, y, old_colour, new_colour) unless col == 1
 
-        # bottom neighbour
-        unless row == rows # If we're not already at the bottom row
-            neighbours << [x + 1, y] if pixel(x + 1, y) == colour
-        end
-
-        # left neighbour
-        unless col == 1 # If we're not already on the left-most column
-            neighbours << [x, y - 1] if pixel(x, y - 1) == colour
-        end
-
-        # right neighbour
-        unless col == cols # If we're not already on the right most column
-            neighbours << [x, y + 1] if pixel(x, y + 1) == colour
-        end
-
-        neighbours_found(x, y) # Mark that we've found neighbours for this pixel
-        neighbours
+        # Fill pixel to the right
+        flood_fill(x + 1, y, old_colour, new_colour) unless col == right_most
     end
 
     def pixel(x, y) # Note, 0-indexed lookup
-        @grid[x][y]
-    end
-
-    def neighbours_found(x, y)
-        @found_neighbours << [x, y]
-    end
-
-    def neighbours_found?(x, y)
-        @found_neighbours.index([x, y]) ? true : false
-    end
-
-    def found_all_neighbours?
-        @neighbours_found.sort == @neighbours.sort
-        binding.pry
+        @grid[y][x]
     end
 end
-
-
-
-
-
-
-
-
-
-
