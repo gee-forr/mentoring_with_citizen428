2640b94d2faf0cd5503b1a0521c641ccbab497c7
Gabriel Fortuna <gee.forr@gmail.com>
2011-10-09 13:57:55 +0200

    Added monkey patched prompt

--- exercise03_image_editor/bin/image_editor_cli.rb
+++ exercise03_image_editor/bin/image_editor_cli.rb
@@ -1,2 +1,33 @@
 #!/usr/bin/env ruby
 
+require '../lib/image_editor'
+
+class String
+  def prompt(options = {})
+    send(options.has_key?(:same_line) ? :print : :puts, self)
+    
+    user_input = IO::gets_with_options(options)
+    return user_input unless options.has_key? :regex # We still have some work to do.
+    
+    # If we're here, it's cos we still need to check user input against a regex
+    while user_input !~ /#{options[:regex]}/
+      puts "Sorry, that input was not accepted, please try again."
+      user_input = self.gets_with_options(options)
+    end
+    
+    user_input # Returning it at this point, as return val might be ambiguous by now
+  end
+  
+end
+
+class IO
+  def self.gets_with_options(options = {})
+    user_input = gets
+    user_input.chomp! unless options.has_key? :no_chomp
+    user_input.strip! if     options.has_key? :strip # Strip should override options like chomp                                                               
+    
+    user_input # A return appears to be necessary here.
+  end
+end
+
+
