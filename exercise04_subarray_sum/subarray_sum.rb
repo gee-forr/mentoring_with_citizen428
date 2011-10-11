e1e5c19191c10fbe6212428c255827b1f05a3fdd
Gabriel Fortuna <gee.forr@gmail.com>
2011-10-11 15:54:42 +0200

    Started doing actual coding

--- exercise04_subarray_sum/subarray_sum.rb
+++ exercise04_subarray_sum/subarray_sum.rb
@@ -1,4 +1,14 @@
 class SubArraySum
   def self.biggest_subarray(array)
+    largest_found = 0
+    largest_array = []
+
+    (1..array.length).each do |cons|
+      array.each_cons do |subarray|
+        next if subarray.sort[-1] <= 0 # No positive numbers in this array.
+
+        #largest_found = 
+      end
+    end
   end
 end
