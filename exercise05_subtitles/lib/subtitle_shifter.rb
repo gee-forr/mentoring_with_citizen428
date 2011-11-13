cd52d8051db99e786596e53537e7bf19da76a608
Gabriel Fortuna <gee.forr@gmail.com>
2011-11-13 18:23:33 +0200

    Started work on building tests

---
+++ exercise05_subtitles/lib/subtitle_shifter.rb
@@ -0,0 +1,2 @@
+class SubtitleShifter
+end
---
+++ exercise05_subtitles/spec/subtitle_shifter_spec.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'minitest/spec'
+require 'minitest/autorun'
+
+require './lib/subtitle_shifter'
+
+describe SubtitleShifter do
+  it "should have a new method" do
+    SubtitleShifter.must_respond_to('new')
+  end
+
+  describe "parsing a subtitle file" do
+    srt = SubtitleShifter.new('The.Big.Bang.Theory.srt')
+
+    it "should work with utf-8 files" do
+      srt.parsed_ok.must_be true
+    end
+
+    it "should have some parsed data" do
+      first_sub = srt.subtitles.first
+      first_sub.must_be_instance_of Hash
+
+      first_sub[:index].must_equal 46
+      first_sub[:text].must_equal "♪ Our whole universee\nwas in a hot, dense state ♪"
+    end
+  end
+end
+
