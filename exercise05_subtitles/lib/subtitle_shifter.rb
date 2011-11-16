30bf11390c01ae409410519a41c226a6d50e0597
Gabriel Fortuna <gee.forr@gmail.com>
2011-11-16 20:13:04 +0200

    Code is feature complete!
    Tests written.
    Just need to write cmd line util and gemify this puppy.

--- exercise05_subtitles/lib/subtitle_shifter.rb
+++ exercise05_subtitles/lib/subtitle_shifter.rb
@@ -11,7 +11,10 @@ class SubtitleShifter
   end
 
   def parse
-    raw_text       = IO.read @sub_file
+    raw_text  = File.open(@sub_file, 'r').read.force_encoding('UTF-8')
+    raw_text.gsub!("\xEF\xBB\xBF".force_encoding("UTF-8"), '') #Remove stupid BOM that was causing me so much grief!
+
+    #raw_text       = IO.read @sub_file
     subtitle_parts = raw_text.split "#{@linebreak}#{@linebreak}"
     @subtitles     = {}
 
@@ -19,13 +22,21 @@ class SubtitleShifter
       @subtitles.update extract_sub_data subtitle
     end
 
-    fix_first_index   # What a hack :(
+    # No longer needed due to removal of BOM
+    #fix_first_index   # What a hack :(
     @parsed_ok = true # Not very useful, but will help when error checking is added
   end
 
   def shift(args)
-    index = args[:index]
+    first = args[:index] # used for checking first go round.
+    index = first
     shift = args[:time]
+
+    if shift < 0 # backward shift check
+      time1 = @subtitles[first][:start] + shift
+      time2 = @subtitles[first-1][:end]
+      raise RuntimeError, 'Cannot overlap backward shift' if time2 > time1
+    end
 
     loop do
       break unless @subtitles.has_key?(index)
@@ -34,8 +45,22 @@ class SubtitleShifter
       @subtitles[index][:end]   += shift
 
       index += 1
-
     end
+  end
+
+  def to_s
+    raise RuntimeError, 'File has not been parsed yet' unless @parsed_ok
+
+    output = ''
+
+    @subtitles.sort.map do |index, sub| 
+      start = ms_to_srt_time sub[:start]
+      fin   = ms_to_srt_time sub[:end]
+
+      output += "#{index}#{@linebreak}#{start} #{TIME_SEPERATOR} #{fin}#{@linebreak}#{sub[:subtitle]}#{@linebreak}#{@linebreak}"
+    end
+
+    output.chomp
   end
 
   private
@@ -52,7 +77,7 @@ class SubtitleShifter
     }
   end
 
-  def srt_time_to_ms (srt_time)
+  def srt_time_to_ms(srt_time)
     time_parts = parse_srt_time srt_time
 
     hours_ms = time_parts[:hours] * 60 * 60 * 1000
@@ -60,6 +85,15 @@ class SubtitleShifter
     secs_ms  = time_parts[:secs]  * 1000
 
     hours_ms + mins_ms + secs_ms + time_parts[:ms]
+  end
+
+  def ms_to_srt_time(ms)
+    hours   = (ms / (1000 *60 *60)) % 60
+    minutes = (ms / (1000 *60) )    % 60
+    seconds = (ms / 1000)           % 60
+    adj_ms  = ms.to_s[-3..-1].to_i
+
+    "%02d:%02d:%02d,%03d" % [hours, minutes, seconds, adj_ms]
   end
 
   def parse_srt_time (srt_time)
@@ -74,6 +108,7 @@ class SubtitleShifter
     }
   end
 
+  # No longer needed due to fixing the BOM issue. But I'm leaving it in.
   def fix_first_index
     # This makes me feel *so* dirty :/
     sub_arr = @subtitles.to_a
@@ -83,7 +118,3 @@ class SubtitleShifter
     @subtitles[idx2 - 1] = @subtitles.delete idx1 # At least I learnt this trick :) How to rename a hash key
   end
 end
-
-
-
-
--- exercise05_subtitles/spec/subtitle_shifter_spec.rb
+++ exercise05_subtitles/spec/subtitle_shifter_spec.rb
@@ -59,21 +59,48 @@ describe SubtitleShifter do
 
     describe "backward" do
       it "should shift subtitles backward from an index" do
-        @srt.must_respond_to('shift')
-
         # Test start of shift
-        @srt.shift(:index => 50, :time => -2500)
-        @srt.subtitles[50][:start].must_equal 182238
-        @srt.subtitles[50][:end].must_equal   184872
+        @srt.shift(:index => 50, :time => -1)
+        @srt.subtitles[50][:start].must_equal 184737
+        @srt.subtitles[50][:end].must_equal   187371
       end
 
       it "should affect all indexes till the end" do
         # start = @srt
         # 53 - s - 191962 e - 192710
-        @srt.shift(:index => 50, :time => -2500)
-        @srt.subtitles[53][:start].must_equal 189462
-        @srt.subtitles[53][:end].must_equal   190210
+        @srt.shift(:index => 50, :time => -1)
+        @srt.subtitles[53][:start].must_equal 191961
+        @srt.subtitles[53][:end].must_equal   192709
       end
+
+      it "should raise an exception if shifted too far back" do
+        proc do
+          @srt.shift(:index => 50, :time => -2500)
+        end.must_raise RuntimeError
+      end
+    end
+  end
+
+  describe "outputting an srt file" do
+    before(:each) do
+      @srt = SubtitleShifter.new('./spec/The.Big.Bang.Theory.srt')
+    end
+
+    it "should raise an exception if it hasn't been parsed yet" do
+      proc do
+        @srt.to_s
+      end.must_raise RuntimeError
+    end
+
+    it "should provide a string that is similar to the file when no shifting takes place" do
+      @srt.parse
+      @srt.to_s.must_equal "46\r\n00:02:53,610 --> 00:02:57,196\r\n♪ Our whole universe\r\nwas in a hot, dense state ♪\r\n\r\n47\r\n00:02:57,198 --> 00:03:00,532\r\n♪ Then nearly 14 billion years\r\nago expansion started... Wait! ♪\r\n\r\n48\r\n00:03:00,534 --> 00:03:02,184\r\n♪ The Earth began to cool ♪\r\n\r\n49\r\n00:03:02,186 --> 00:03:04,736\r\n♪ The autotrophs began to drool,\r\nNeanderthals developed tools ♪\r\n\r\n50\r\n00:03:04,738 --> 00:03:07,372\r\n♪ We built the Wall ♪\r\n♪ <i>We built the pyramids</i> ♪\r\n\r\n51\r\n00:03:07,374 --> 00:03:10,058\r\n♪ Math, Science, History,\r\nunraveling the mystery ♪\r\n\r\n52\r\n00:03:10,060 --> 00:03:11,960\r\n♪ That all started\r\nwith a big bang ♪\r\n\r\n53\r\n00:03:11,962 --> 00:03:12,710\r\n♪ <i>Bang!</i> ♪\r\n"
+    end
+
+    it "should provide a string that shows shifted times when a shift op occurs" do
+      @srt.parse
+      @srt.shift(:index => 50, :time => 2500)
+      @srt.to_s.must_equal "46\r\n00:02:53,610 --> 00:02:57,196\r\n♪ Our whole universe\r\nwas in a hot, dense state ♪\r\n\r\n47\r\n00:02:57,198 --> 00:03:00,532\r\n♪ Then nearly 14 billion years\r\nago expansion started... Wait! ♪\r\n\r\n48\r\n00:03:00,534 --> 00:03:02,184\r\n♪ The Earth began to cool ♪\r\n\r\n49\r\n00:03:02,186 --> 00:03:04,736\r\n♪ The autotrophs began to drool,\r\nNeanderthals developed tools ♪\r\n\r\n50\r\n00:03:07,238 --> 00:03:09,872\r\n♪ We built the Wall ♪\r\n♪ <i>We built the pyramids</i> ♪\r\n\r\n51\r\n00:03:09,874 --> 00:03:12,558\r\n♪ Math, Science, History,\r\nunraveling the mystery ♪\r\n\r\n52\r\n00:03:12,560 --> 00:03:14,460\r\n♪ That all started\r\nwith a big bang ♪\r\n\r\n53\r\n00:03:14,462 --> 00:03:15,210\r\n♪ <i>Bang!</i> ♪\r\n"
     end
   end
 end
