9aa0e49a0caa4f2b8b04172e3e277d3be5c7f04a
Gabriel Fortuna <gee.forr@gmail.com>
2012-01-04 15:40:39 +0200

    Added fib tests up to sequence 20

--- exercise06_memoized_fibonacci/spec/fibonacci_spec.rb
+++ exercise06_memoized_fibonacci/spec/fibonacci_spec.rb
@@ -7,18 +7,32 @@ require 'minitest/autorun'
 require './lib/fibonacci'
 
 fibs = {
-  0 => 0,
-  1 => 1,
-  2 => 1,
-  3 => 2,
-  4 => 3,
-  5 => 5,
-  6 => 8,
+  0  => 0,
+  1  => 1,
+  2  => 1,
+  3  => 2,
+  4  => 3,
+  5  => 5,
+  6  => 8,
+  7  => 13,
+  8  => 21,
+  9  => 34,
+  10 => 55,
+  11 => 89,
+  12 => 144,
+  13 => 233,
+  14 => 377,
+  15 => 610,
+  16 => 987,
+  17 => 1597,
+  18 => 2584,
+  19 => 4181,
+  20 => 6765
 }
 
 def fib_seq_test_name(n, m)
   # Just so I don't have to type out the whole test title again and again
-  "should result in #{n} for fib sequence #{m}"
+  "should result in #{m} for fib sequence #{n}"
 end
 
 describe Fibonacci do
