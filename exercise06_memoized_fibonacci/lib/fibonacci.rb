68da59821ff0db1ee73b3af95a25f157e98460f3
Gabriel Fortuna <gee.forr@gmail.com>
2012-01-06 12:47:36 +0200

    changed class var to class instance var

--- exercise06_memoized_fibonacci/lib/fibonacci.rb
+++ exercise06_memoized_fibonacci/lib/fibonacci.rb
@@ -1,11 +1,13 @@
 class Fibonacci
   
-  @@fibs = {}
-
+  class << self; attr_accessor :fibs end
+  @fibs = {}
+  
   def self.fib(n)
-    return n         if n <= 1
-    return @@fibs[n] if @@fibs.has_key? n
+    return n       if n <= 1
+    return fibs[n] if fibs.has_key? n
 
-    @@fibs[n] = fib(n - 1) + fib(n - 2)
+    fibs[n] = fib(n - 1) + fib(n - 2)
   end
+    
 end
